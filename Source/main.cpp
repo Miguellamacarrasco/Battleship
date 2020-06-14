5f8e4e4f28ede968a68fe6055de686bc6e3f2926
Miguel Lama <migue.lama@yahoo.com>
2020-06-14 16:54:13 -0500

    Cambios

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,19 +1,55 @@
 #include "head.h"
 
-
-std::pair<int,char> generate_ship_coords(std::vector<std::vector<int>> empty_spaces)
+int find_consecutive_free_spaces(std::vector<int> vect)
 {
-	int random_y = rand() % 9;
-	while (!(empty_spaces[random_y].size()))
+	int highest_consecutive_spaces = 0;
+	int spaces = 0;
+	int counting = vect[0];
+	for (int i = 1; i<vect.size(); i++)
+	{
+		if ((counting + 1) == vect[i])
+		{
+			spaces++;
+		}
+		else
+		{
+			if (highest_consecutive_spaces < spaces)
+			{
+				highest_consecutive_spaces = spaces;
+			}
+		}
+		counting = vect[i];
+	}
+	return highest_consecutive_spaces;
+}
+
+std::pair<int,char> generate_ship_coords(std::vector<std::vector<int>> empty_spaces, ship* boat)
+{
+	int horizontal = rand() % 2;
+	if (horizontal)
 	{
 		int random_y = rand() % 9;
+		while (find_consecutive_free_spaces(empty_spaces[random_y]) < boat->size_of() + 2)
+		{
+			int random_y = rand() % 9;
+		}
+		int random_x = rand() % empty_spaces.size();
 	}
+	
 }
 
 
 int main()
 
 {
+	std::vector<std::vector<int>> free_spaces;
+	for(auto vect1 : free_spaces)
+	{
+		for (int i=0;i<free_spaces.size();i++)
+		{
+			vect1[i] = i;
+		}
+	}
 	srand(time(NULL));
 	return 0;
 }
